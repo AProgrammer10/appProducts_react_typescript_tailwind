082b215ce5869d65e54c09bc1ff49b4398337ef5
ArtemKichigin <kevlar101@mail.ru>
2022-09-24 00:46:23 +0300

    createContext<IModalContext>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useContext} from 'react'
 import {Navbar} from './components/Navbar'
 import {Product} from './components/Product'
 import {Loader} from './components/Loader'
@@ -7,26 +7,27 @@ import { ErrorMessage } from './components/ErrorMessage'
 import { ModalWindow } from './components/ModalWindow'
 import {CreateProduct} from './components/CreateProduct'
 import {IProduct} from './types/interfaces'
+import {ModalContext} from './context/ModalContext'
 
 function App(){
   const {products, loading, error, addProduct} = useProducts()
-  const [modal, setModal] = useState(false)
+  const {modal, open, close} = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    close()
     addProduct(product)
 
   }
 
   return (
     <div className="container mx-auto max-w-2xl pt-5 items-center">
-      <Navbar onClick={() => setModal(true)}/>
+      <Navbar onClick={() => open()}/>
       {/*<button className="rounded mt-5 px-5 py-2 bg-green-400 text-red-900 outline-0" onClick={() => setModal(true)}>Create a new product</button>*/}
       {loading && <Loader />}
       {error && <ErrorMessage error={error} />}
       {products.map(product => <Product product={product} key={product.id} />)}
 
-      {modal && <ModalWindow title="Create new product" onClose={() => setModal(false)}>
+      {modal && <ModalWindow title="Create new product" onClose={() => close()}>
         <CreateProduct onCreate={createHandler} />
       </ModalWindow>}
     </div>
